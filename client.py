fee4e17e17ce5724da4936797bd9f1024a4769e6
ET+ALEX CryptoWork <CryptoWork@Mais-MacBook-Pro.local>
2018-04-11 14:30:31 +0800

    created entities

---
+++ client.py
@@ -0,0 +1,8 @@
+import json, requests
+from pprint import pprint 
+
+from sw_entities import Person, Planet, Vehicle, Starship, Species
+from base import DbManager
+
+db = DbManager()
+
---
+++ sw_entities.py
@@ -0,0 +1,86 @@
+from sqlalchemy.orm import relationship, backref, joinedload
+from sqlalchemy import Column, DateTime, String, Integer, Float, ForeignKey, func
+
+from base import Base, inverse_relationship, create_tables
+
+class Person(Base):
+    __tablename__ = 'people'
+    id = Column(Integer, primary_key=True)
+
+    url = Column(String, unique=True)
+    
+    name = Column(String)
+    height = Column(String)
+    mass = Column(String)
+    hair_color = Column(String)
+    skin_color = Column(String)
+    
+    created_at = Column(DateTime, default=func.now())
+    updated_at = Column(DateTime, default=func.now(), onupdate=func.now())
+
+
+class Planet(Base):
+    __tablename__ = 'planet'
+    id = Column(Integer, primary_key=True)
+
+    url = Column(String, unique=True)
+    
+    name = Column(String)
+    rotation_period = Column(String)
+    orbital_period = Column(String)
+    diameter = Column(String)
+    climate = Column(String)
+    
+    created_at = Column(DateTime, default=func.now())
+    updated_at = Column(DateTime, default=func.now(), onupdate=func.now())
+
+
+class Vehicle(Base):
+    __tablename__ = 'vehicle'
+    id = Column(Integer, primary_key=True)
+
+    url = Column(String, unique=True)
+    
+    name = Column(String)
+    model = Column(String)
+    manufacturer = Column(String)
+    cost_in_credits = Column(String)
+    length = Column(String)
+    
+    created_at = Column(DateTime, default=func.now())
+    updated_at = Column(DateTime, default=func.now(), onupdate=func.now())
+
+
+class Starship(Base):
+    __tablename__ = 'starship'
+    id = Column(Integer, primary_key=True)
+
+    url = Column(String, unique=True)
+    
+    name = Column(String)
+    model = Column(String)
+    manufacturer = Column(String)
+    cost_in_credits = Column(String)
+    length = Column(String)
+    
+    created_at = Column(DateTime, default=func.now())
+    updated_at = Column(DateTime, default=func.now(), onupdate=func.now())
+
+
+class Species(Base):
+    __tablename__ = 'species'
+    id = Column(Integer, primary_key=True)
+
+    url = Column(String, unique=True)
+    
+    name = Column(String)
+    classification = Column(String)
+    designation = Column(String)
+    average_height = Column(String)
+    skin_colors = Column(String)
+    
+    created_at = Column(DateTime, default=func.now())
+    updated_at = Column(DateTime, default=func.now(), onupdate=func.now())
+
+if __name__ != '__main__':
+    create_tables()
