4ba6bf7a1edd3dceba16bbf310e6e71f6d15e182
Peter <pesomerville@peter-somervilles-macbook-pro.local>
2018-04-11 16:37:51 +0800

    added methods

--- client.py
+++ client.py
@@ -11,21 +11,16 @@ def get_json(url):
     response = requests.get(url)
     return json.loads(response.text)
 
-for person in range(1,87):
+for person in range(20,30):
     person_url = 'https://swapi.co/api/people/{}'.format(person)
     results = db.open().query(Person).filter(Person.url == person_url).all()
-    if len(results) == 0:
+    if len(results) == 0:   
         
         json_data = get_json(person_url)
 
         person = Person()
         if 'detail' not in json_data:
-            person.name = json_data["name"]
-            person.url = person_url
-            person.height = json_data["height"]
-            person.mass = json_data["mass"]
-            person.hair_color = json_data["hair_color"]
-            person.skin_color = json_data["skin_color"]
+            person.parse_person(json_data)
 
             db.save(person)
 
@@ -37,33 +32,23 @@ for planet in range(1,10):
         json_data = get_json(planet_url)
 
         planet = Planet()
+        if 'detail' not in json_data:
+            planet.parse_planet(json_data)
+
+            db.save(planet)
+
+for vehicle in range(14,24):
+    vehicle_url = 'https://swapi.co/api/vehicles/{}'.format(vehicle)
+    results = db.open().query(Vehicle).filter(Vehicle.url == vehicle_url).all()
+    if len(results) == 0:
     
-        planet.name = json_data["name"]
-        planet.url = planet_url
-        planet.rotation_period = json_data["rotation_period"]
-        planet.orbital_period = json_data["orbital_period"]
-        planet.diameter = json_data["diameter"]
-        planet.climate = json_data["climate"]
+        json_data = get_json(vehicle_url)
 
-        db.save(planet)
-
-
-vehicle_url = 'https://swapi.co/api/vehicles/14'
-results = db.open().query(Vehicle).filter(Vehicle.url == vehicle_url).all()
-if len(results) == 0:
+        vehicle = Vehicle()
+        if 'detail' not in json_data:
+            vehicle.parse_vehicle(json_data)
     
-    json_data = get_json(vehicle_url)
-
-    vehicle = Vehicle()
-    
-    vehicle.name = json_data["name"]
-    vehicle.url = vehicle_url
-    vehicle.model = json_data["model"]
-    vehicle.manufacturer = json_data["manufacturer"]
-    vehicle.cost_in_credits = json_data["cost_in_credits"]
-    vehicle.length = json_data["length"]
-
-    db.save(vehicle)
+            db.save(vehicle)
 
 
 starship_url = 'https://swapi.co/api/starships/12'
@@ -73,15 +58,10 @@ if len(results) == 0:
     json_data = get_json(starship_url)
 
     starship = Starship()
-    
-    starship.name = json_data["name"]
-    starship.url = starship_url
-    starship.model = json_data["model"]
-    starship.manufacturer = json_data["manufacturer"]
-    starship.cost_in_credits = json_data["cost_in_credits"]
-    starship.length = json_data["length"]
+    if 'detail' not in json_data:
+        starship.parse_starship(json_data)
 
-    db.save(starship)
+        db.save(starship)
 
 
 species_url = 'https://swapi.co/api/species/1'
@@ -91,13 +71,8 @@ if len(results) == 0:
     json_data = get_json(species_url)
 
     species = Species()
+    if 'detail' not in json_data:
+        species.parse_species(json_data)
     
-    species.name = json_data["name"]
-    species.url = species_url
-    species.classification = json_data["classification"]
-    species.designation = json_data["designation"]
-    species.average_height = json_data["average_height"]
-    species.skin_colors = json_data["skin_colors"]
-
-    db.save(species)
+        db.save(species)
 
--- sw_entities.py
+++ sw_entities.py
@@ -19,11 +19,12 @@ class Person(Base):
     updated_at = Column(DateTime, default=func.now(), onupdate=func.now())
 
     def parse_person(self, json_data):
-        
-
-
-    
-
+        self.name = json_data['name']
+        self.url = json_data['url']
+        self.height = json_data['height']
+        self.mass = json_data['mass']
+        self.hair_color = json_data['hair_color']
+        self.skin_color = json_data['skin_color']
 
 
 class Planet(Base):
@@ -41,6 +42,14 @@ class Planet(Base):
     created_at = Column(DateTime, default=func.now())
     updated_at = Column(DateTime, default=func.now(), onupdate=func.now())
 
+    def parse_planet(self, json_data):
+        self.name = json_data['name']
+        self.url = json_data['url']
+        self.rotation_period = json_data['rotation_period']
+        self.orbital_period = json_data['orbital_period']
+        self.diameter = json_data['diameter']
+        self.climate = json_data['climate']
+
 
 class Vehicle(Base):
     __tablename__ = 'vehicle'
@@ -57,6 +66,13 @@ class Vehicle(Base):
     created_at = Column(DateTime, default=func.now())
     updated_at = Column(DateTime, default=func.now(), onupdate=func.now())
 
+    def parse_vehicle(self, json_data):
+        self.name = json_data['name']
+        self.url = json_data['url']
+        self.model = json_data['model']
+        self.manufacturer = json_data['manufacturer']
+        self.cost_in_credits = json_data['cost_in_credits']
+        self.length = json_data['length']
 
 class Starship(Base):
     __tablename__ = 'starship'
@@ -73,6 +89,13 @@ class Starship(Base):
     created_at = Column(DateTime, default=func.now())
     updated_at = Column(DateTime, default=func.now(), onupdate=func.now())
 
+    def parse_starship(self, json_data):
+        self.name = json_data['name']
+        self.url = json_data['url']
+        self.model = json_data['model']
+        self.manufacturer = json_data['manufacturer']
+        self.cost_in_credits = json_data['cost_in_credits']
+        self.length = json_data['length']
 
 class Species(Base):
     __tablename__ = 'species'
@@ -88,6 +111,14 @@ class Species(Base):
     
     created_at = Column(DateTime, default=func.now())
     updated_at = Column(DateTime, default=func.now(), onupdate=func.now())
+
+    def parse_species(self, json_data):
+        self.name = json_data['name']
+        self.url = json_data['url']
+        self.classification = json_data['classification']
+        self.designation = json_data['designation']
+        self.average_height = json_data['average_height']
+        self.skin_colors = json_data['skin_colors']
 
 if __name__ != '__main__':
     create_tables()
